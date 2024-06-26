b9458706da5df95dc5790a006fd052d56a5cdedf
Mohammad Munzir <mohammadmunzir2k@gmail.com>
2024-06-26 20:36:14 +0530

    final commit

---
+++ modules/room/components/chat/Message.tsx
@@ -0,0 +1,33 @@
+import { socket } from '@/common/lib/socket';
+
+const Message = ({ userId, msg, username, color }: Message) => {
+	const me = socket.id === userId;
+
+	return (
+		<div
+			className={`my-2 flex gap-2 text-clip ${
+				me && 'font justify-end text-right'
+			}`}
+		>
+			{me && (
+				<h5
+					style={{ color }}
+					className='font-bold'
+				>
+					You:
+				</h5>
+			)}
+			{!me && (
+				<h5
+					style={{ color }}
+					className='font-bold'
+				>
+					{username}:
+				</h5>
+			)}
+			<p style={{ wordBreak: 'break-all' }}>{msg}</p>
+		</div>
+	);
+};
+
+export default Message;
---
+++ pages/_document.tsx
@@ -0,0 +1,28 @@
+import { Html, Main, NextScript, Head } from 'next/document';
+
+const document = () => (
+	<Html>
+		<Head>
+			<link
+				rel='preconnect'
+				href='https://fonts.googleapis.com'
+			/>
+			<link
+				rel='preconnect'
+				href='https://fonts.gstatic.com'
+				crossOrigin='allow'
+			/>
+			<link
+				href='https://fonts.googleapis.com/css2?family=Bitter:wght@100;200;300;400;500;600;700;800;900&display=swap'
+				rel='stylesheet'
+			/>
+		</Head>
+		<body>
+			<div id='portal'></div>
+			<Main />
+			<NextScript />
+		</body>
+	</Html>
+);
+
+export default document;
